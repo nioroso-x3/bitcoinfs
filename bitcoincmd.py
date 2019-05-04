db616debe11774067a03352241d60fcc2ccd2541
Judemir Ribeiro <nioroso_x3@hotmail.com>
2019-05-04 11:54:51 -0400

    fixes

--- bitcoincmd.py
+++ bitcoincmd.py
@@ -5,6 +5,17 @@ import subprocess as sb
 import binascii
 import sys
 import json
+def txid2boptreturn(txid):
+  ''' return OP_RETURN in bytes given a txid'''
+  literal1 = 'bitcoin-cli gettransaction ' + str(txid)
+  bsv1 = subprocess.check_output(literal1,shell=True,stderr=subprocess.STDOUT,).decode("utf-8").strip('\x00')
+  jbsv1 = json.loads(bsv1)
+  literal2 = 'bitcoin-cli decoderawtransaction ' + jbsv1['hex']
+  bsv2 = subprocess.check_output(literal2,shell=True,stderr=subprocess.STDOUT,).decode("utf-8").strip('\x00')
+  jbsv2= json.loads(bsv2)
+  opreturnhex=jbsv2['vout'][0]['scriptPubKey']['asm'].split('OP_RETURN')[1]
+  return bytes.fromhex(opreturnhex)
+
 def str2hex(data):
   if type(data) == str:
     return binascii.hexlify(data.encode('utf-8'))
@@ -35,14 +46,13 @@ def btcGetBalance():
   balance = call(["bitcoin-cli","getbalance"])
   return int(float(balance)*btc2sat)
   
-def btcGenOPRETURN(data,txfee=0.0001):
+def btcGenOPRETURN(data,txfee=0.0005):
   txid,vout = btcGetUnspent()
   change = str(float(btcGetBalance() - txfee*btc2sat)/btc2sat)
   addr = btcNewAddress() 
-  inputs = "\"[{\\\"txid\\\":\\\"%s\\\",\\\"vout\\\":%s}]\"" % (txid,vout)
-  outputs = "\"{\\\"data\\\":\\\"%s\\\",\\\"%s\\\":%s}\"" % (str(str2hex(data),"ascii"),addr,change)
-
-  tx = sb.check_output("/usr/local/bin/bitcoin-cli createrawtransaction "+inputs+" "+outputs,shell=True,executable="/bin/bash" ).decode("utf-8").strip()
+  tx = call(["bitcoin-tx","-create","in=%s:%s"%(txid,vout),
+                                    "outdata=%s" % (str(str2hex(data))),
+                                    "outaddr=%s:%s" %(change,addr.split(":")[1])])
   return tx
 
 def btcSignTx(tx):
