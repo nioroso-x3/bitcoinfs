1d31b09122a1d840fe77e6c6924c509f6125e774
Judemir Ribeiro <nioroso_x3@hotmail.com>
2019-05-06 00:43:36 -0400

    Finished file upload script

--- file2opreturn.sh
+++ file2opreturn.sh
@@ -3,4 +3,40 @@
 userpass=$1
 f=$2
 startN=$3
-out=$(./file2opreturn.py ${userpass} $
+out=$(./file2opreturn.py ${userpass} ${f} ${startN})
+if echo $out | grep -q 'Success' ;
+then
+    v=(${out})
+    endN=${v[2]}
+    startN=$((startN + 1))
+    echo $out
+elif echo $out | grep -q 'Fail wait for new utxo' 
+then
+    echo "No available utxos"
+    exit
+else
+    echo "Other error occurred"
+    exit
+fi
+
+for i in $(seq $startN $endN)
+do
+    while :
+    do
+        out=$(./file2opreturn.py ${userpass} ${f} ${i})
+        if echo $out | grep -q 'Success' ; 
+        then
+            echo $out
+            break
+        elif echo $out | grep -q 'Fail wait for new utxo' 
+        then
+            echo "Waiting for new utxo, sleeping 30 seconds"
+            sleep 30
+        else
+            echo "Other error ocurred"
+            echo $out
+            exit
+        fi
+    done
+done
+
