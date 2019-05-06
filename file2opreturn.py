f475f454ab3dae6328b9355ca3d95b66a1ec3aa7
Judemir Ribeiro <nioroso_x3@hotmail.com>
2019-05-06 00:09:06 -0400

    fix error messages

--- file2opreturn.py
+++ file2opreturn.py
@@ -71,10 +71,10 @@ if __name__ == "__main__":
             rdysend = btcSendRaw_RPC(sys.argv[1],signed)
             if r > 0:
                 l+=1
-            print(n+1,l,sys.argv[2],rdysend)
+            print("Success",n+1,l,sys.argv[2],rdysend)
         except noutxo:
-            print("Wait for new utxo")
+            print("Fail wait for new utxo")
         except:
-            print("Other error ocurred")
+            print("Fail other error ocurred")
     else:
         print("Wrong number of arguments <user:pass> <file> <chunk N>\n",sys.argv)
